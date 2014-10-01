625bce3eec7a806adb953f5adf84c5fddb8d9dbb
Kendall Flutey <kendall.flutey@abletech.co.nz>
2014-10-01 22:02:51 +1300

    Add roman numeral files

---
+++ roman-numerals.cs
@@ -0,0 +1,12 @@
+public string ToRoman(int number)
+{
+  // Your code here
+}
+
+// Driver code... this should print out trues.
+
+Console.WriteLine(ToRoman(1) == "I");
+Console.WriteLine(ToRoman(3) == "III");
+Console.WriteLine(ToRoman(6) == "VI");
+
+// TODO: what other cases could you add to ensure your to_roman method is working?
